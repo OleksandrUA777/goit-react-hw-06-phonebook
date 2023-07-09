c01b37db39147831020848ffc8c4630af4b68888
Oleksandr <110254286+OleksandrUA777@users.noreply.github.com>
2023-07-09 13:40:22 +0200

    fixed error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 const persistConfig = {
@@ -8,5 +17,13 @@ const persistConfig = {
   storage,
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
-export const store = configureStore({ reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 export const persistor = persistStore(store);
